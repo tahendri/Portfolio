5ee5eaaf20e24883e4d0268e83a30f5da78dcfb2
Travis <Travis>
2016-04-27 14:06:18 -0700

    adding second wheel

--- activityIndicator/ai.js
+++ activityIndicator/ai.js
@@ -1,10 +1,10 @@
 var AI = {
   show: function(style){
-    if style = "style2"
+    if (style = "style2")
     {
 		var mu = "<div class='fadeBox'> <div class='spinner2'> </div> </div>";
 	}
-	else if style = "style3"
+	else if (style = "style3")
 	{
 		var mu = "<div class='fadeBox'> <div class='spinner3'> </div> </div>";
 	}
