ea56c579896ac242c979a414360245da7febc0b2
Travis <Travis>
2016-04-27 13:50:28 -0700

    adding second wheel

--- activityIndicator/ai.js
+++ activityIndicator/ai.js
@@ -3,6 +3,10 @@ var AI = {
     var mu = "<div class='fadeBox'> <div class='spinner'> </div> </div>";
     document.body.innerHTML += mu;
   },
+  show: function(style2){
+	var mu = "<div class='fadeBox'> <div class='spinner2'> </div> </div>";
+	document.body.innerHTML += mu;
+  }
   hide: function(){
     $(".fadeBox").remove();
   }
